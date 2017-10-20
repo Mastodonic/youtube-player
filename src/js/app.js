1153a5e4e0917484abb787fe7a80be5a6437f501
Milad Alizadeh <milad.alizadeh@breakthroughmedia.org>
2017-10-20 13:50:19 +0100

    add docs folder

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,21 @@
 import YoutubePlayer from './youtube-player';
 
-document.querySelectorAll('[data-youtube-id]').forEach((element) => {
-    new YoutubePlayer({
-        element
-    });
+// document.querySelectorAll('[data-youtube-id]').forEach((element) => {
+//     new YoutubePlayer({
+//         element
+//     });
+// });
+
+let yp = new YoutubePlayer({
+    element: document.querySelectorAll('.js-video')[0],
+    coverImageSize: 'maximum',
+    forceCoverOnTouchDevices: false
 });
 
-// new YoutubePlayer({
-//     element: document.querySelectorAll('.js-video')[1],
-//     forceCoverOnTouchDevices: true
-// });
+setTimeout(() => {
+    yp.loadNewVideo({
+        videoUrl: 'https://www.youtube.com/watch?v=qJq8IovXFYQ',
+        coverImageSize: 'medium'
+
+    });
+}, 25000);
